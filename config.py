a30144bbbfcda34b5c62e5efda91e0a0b3e81473
Aleksandr Borisov <56392877+Alex-Borisov92@users.noreply.github.com>
2024-09-19 12:22:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # API токен для авторизации
-ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyNjczMTkyNCwianRpIjoiNTY1OWE1ODEtMjhkNy00NjFjLThhZmItZDUxZGYwYjc0MGJjIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJkYl9jb25uX25hbWUiOiJkYXRhYmFzZSIsInVzZXJfaWQiOjEsIm1hbmFnZXJfaWQiOjEsImlzX2FkbWluIjoxfSwibmJmIjoxNzI2NzMxOTI0LCJleHAiOjE3MjY3MzkxMjR9.NbFmpr1NQCQ82nukAbG3NdjEEUSUq5bnVON3voInAdQ'
+ACCESS_TOKEN = '<insert the token>'
 
 # Настройки базы данных 
 DB_CONFIG = {
